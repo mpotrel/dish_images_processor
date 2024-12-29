b3ffe58c7afc0527d27f44b5593dc3780b2d6e08
ManuPotrel <71026308+ManuPotrel@users.noreply.github.com>
2024-12-29 14:29:31 +0100

    add config for kafka and logging

--- dish_images_processor/models/messages.py
+++ dish_images_processor/models/messages.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class InputImageMessage(BaseModel):
     """Message format for single image processing"""
     job_id: str
     image_url: str
-    submitted_at: datetime
+    created_at: datetime = Field(default_factory=datetime.utcnow)
 
 class OutputImageMessage(InputImageMessage):
     processed_url: str
--- dish_images_processor/routers/v0/prod.py
+++ dish_images_processor/routers/v0/prod.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
+from dish_images_processor.models.messages import InputImageMessage
+
 
 prod_router = APIRouter()
 
 @prod_router.post("/preprocess")
-async def preprocess_img():
+async def preprocess_img(input_img: InputImageMessage):
     pass
